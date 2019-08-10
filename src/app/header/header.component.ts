9c10310b46f918e689fa56f8629d97c7ba230b98
Daniel Moskal <daniel19967@gmail.com>
2019-08-10 15:02:20 +0200

    83, 84, 85 - navigation, recipe-list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+
   collapsed = true;
+
+  @Output()
+  featureSelected = new EventEmitter<string>();
+
+  onSelectRecipies() {
+    this.featureSelected.emit('recipe');
+  }
+
+  onSelectShoppingList() {
+    this.featureSelected.emit('shopping-list');
+  }
 }
