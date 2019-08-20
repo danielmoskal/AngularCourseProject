10cf9c71813ef2134177386b935d1dfc8d7ef0e5
Daniel Moskal <daniel19967@gmail.com>
2019-08-20 22:15:43 +0200

    120 - adding ingredients to recipies

--- src/app/recipies/recipe.service.ts
+++ src/app/recipies/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -8,12 +9,20 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Tomato', 5)
+      ]
     ),
     new Recipe(
       'A test recipe',
       'This is simply a test 2',
-      'https://cdn.pixabay.com/photo/2018/09/14/11/13/food-3676808_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2018/09/14/11/13/food-3676808_960_720.jpg',
+      [
+        new Ingredient('Potato', 10),
+        new Ingredient('Meat', 2)
+      ]
     )
   ];
 
